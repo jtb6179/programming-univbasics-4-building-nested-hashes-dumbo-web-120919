e3cda03cdf9657e3078ca5e3987179e735e4e5be
Joel  Bly <jtb6179@g.rit.edu>
2019-11-27 18:52:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,9 @@ def monopoly_with_third_tier
     :one_piece_owned => 25, :two_pieces_owned =>50,
       :three_pieces_owned =>100,
       :four_pieces_owned =>200},
-  :names => {:reading_railroad => {"mortgage_value"=>"$100"},:pennsylvania_railroad =>{"mortgage_value"=>"$200"},
-    :b_and_o_railroad => {"mortgage_value"=>"$400"}, :shortline_railroad => {"mortgage_value"=>"$800"}}
+  :names => {:reading_railroad => {},:pennsylvania_railroad =>{},
+    :b_and_o_railroad => {}, :shortline_railroad => {}}
+}
 }
 end
 
@@ -22,5 +23,10 @@ def monopoly_with_fourth_tier
       :three_pieces_owned =>100,
       :four_pieces_owned =>200},
   :names => {:reading_railroad => {"mortgage_value"=>"$100"},:pennsylvania_railroad =>{"mortgage_value"=>"$200"},
-    :b_and_o_railroad => {"mortgage_value"=>"$400"}, :shortline_railroad => {"mortgage_value"=>"$800"}}
+    :b_and_o_railroad => {"mortgage_value"=>"$400"}, :shortline_railroad => {"mortgage_value"=>"$800"}
+    
+  }
+    
+  }
+}
 end
